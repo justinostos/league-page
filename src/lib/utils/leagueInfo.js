cca69b12dc55555b8db45d3fbe4813df699da663
justinostos <justin.ostos@yahoo.com>
2026-08-03 09:54:52 -0500

    Update leagueInfo.js
    updated new players

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -118,7 +118,7 @@ export const managers = [
        "managerID": "1266452995407609856",
        "name": "Mahomie8715",
     //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Somewhere, North Carolina", // (optional)
+       "location": "Austin, Texas", // (optional)
     //   "bio": "Lorem ipsum...",
        "photo": "/managers/BatCat.jpg", // square ratio recommended (no larger than 500x500)
     //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
@@ -276,14 +276,14 @@ export const managers = [
       },
   {
        "roster": 12,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "managerID": "1131729942280024064",
-       "name": "SilkyJohnson5264",
+       "managerID": "607622270189400064",
+       "name": "sawoodard",
     //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Kyle, Texas", // (optional)
+       "location": "Austin, Texas", // (optional)
     //   "bio": "Lorem ipsum...",
        "photo": "/managers/Bear.jpg", // square ratio recommended (no larger than 500x500)
     //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-       "favoriteTeam": "hou", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     //   "rival": {
     //     name: "Rival", // Can be anything (usually your rival's name)
